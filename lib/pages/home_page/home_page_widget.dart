4a5d5e950c2e1bf8b29b02e47ba67bb17646c12d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-20 18:25:17 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -346,7 +346,7 @@ class _HomePageWidgetState extends State<HomePageWidget>
                           padding: EdgeInsetsDirectional.fromSTEB(
                               0.0, 10.0, 0.0, 0.0),
                           child: Text(
-                            'My Result : ${(_model.apiResultowq?.getHeader('Header : ') ?? '')}${(_model.apiResultowq?.bodyText ?? '')}',
+                            'My Result : Hearder : ${(_model.apiResultowq?.getHeader('Header : ') ?? '')}Body 1${(_model.apiResultowq?.bodyText ?? '')}Body 2${(_model.apiResultowq?.statusCode ?? 200).toString()}',
                             style: FlutterFlowTheme.of(context).bodyMedium,
                           ),
                         ),
